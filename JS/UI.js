84d6fa6d605a1398a5a6f8db6d46be8a9f3be0d7
Yousef ibrahim <108943127+iioyx@users.noreply.github.com>
2024-06-30 15:02:57 +0300

    Add files via upload

---
+++ JS/UI.js
@@ -0,0 +1,63 @@
+export class UI {
+  constructor(data) {
+    this.data = data;
+  }
+
+  displaygames(result) {
+    let box = ``;
+    for (let i = 0; i < result.length; i++) {
+      box += `
+        <div id="${result[i].id}" class="item col-lg-3 col-md-4 col-sm-12 px-2 mb-3">
+            <div class="card bg-transparent text-white px-2 ">
+                <img class="mt-3 rounded" src="${
+                  result[i].thumbnail
+                }" alt="Pic">
+                <div class="mt-3 d-flex justify-content-between align-items-center">
+                    <h5 class="card-title">${result[i].title}</h5>
+                    <p class="price">Free</p>
+                </div>
+                <div class="desc m-0 text-center mb-3">${
+                  result[i].short_description.length > 50
+                    ? result[i].short_description.slice(0, 55) + "..."
+                    : result[i].short_description
+                }</div>
+                <div class="card-footer px-0 d-flex justify-content-between">
+                    <p class="card-footer-item mb-0">MMORGP</p>
+                    <p class="card-footer-item mb-0">${result[i].platform}</p>
+                </div>
+            </div>
+        </div>
+      `;
+    }
+    document.getElementById("data").innerHTML = box;
+    
+  }
+
+  displaydetails(game) {
+    let box = `
+    
+      <div class= "container vh-100 ">
+      <div class="h-100 row gy-1">
+      
+      <div class= "d-flex justify-content-between align-items-center text-white">
+        <p class = "fs-3">Game Details :</p>
+        <i id="icon" class="icon fa-solid fa-circle-xmark fs-3 text-white"></i>
+    </div>
+
+      <div class="col-md-4 m-0">
+      <img src="${game.thumbnail}" class="w-100" alt="image details" />
+    </div>
+    <div class="col-md-7 text-white m-0" >
+      <h3>Title : ${game.title}</h3>
+      <p>Category : <span class="badge text-bg-info"> ${game.genre}</span> </p>
+      <p>Platform : <span class="badge text-bg-info"> ${game.platform}</span> </p>
+      <p class="small">${game.short_description}</p>
+      <a class="btn btn-outline-warning" target="_blank" href="${game.game_url}">Show Game</a>
+   </div>
+      </div>
+      </div>
+      `;
+    document.getElementById('data').innerHTML = box;
+}
+
+}
---
+++ JS/games.js
@@ -0,0 +1,75 @@
+// games.js
+
+import { UI } from "./UI.js";
+
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
+
+
+export class Games {
+
+  constructor() {
+    this.links = document.querySelectorAll(".nav-link");
+    this.newUI = new UI();
+    this.getCategory();
+    this.getApi("mmorpg");
+  }
+
+  async getApi(category = "mmorpg") {
+    const url = `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${category}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "9dc24ac970msh268d498100e2e38p10f795jsn0837d2a686b3",
+        "x-rapidapi-host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      this.newUI.displaygames(result);
+      this.showdetails(result); 
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  getCategory() {
+    for (let i = 0; i < this.links.length; i++) {
+      this.links[i].addEventListener("click", (e) => {
+        this.getApi(this.links[i].textContent.toLowerCase());
+        document.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+      });
+    }
+  }
+
+  showdetails(result) {
+    const cards = document.querySelectorAll(".item");
+    cards.forEach(card => {
+        card.addEventListener("click", (e) => {
+            const id = e.currentTarget.getAttribute("id");
+            const game = result.find((game) => game.id == id);
+            if (game) {
+                nav.classList.add('d-none');
+                header.classList.add('d-none');
+                this.newUI.displaydetails(game);
+            
+                document.getElementById('icon').addEventListener('click', () => {
+                    nav.classList.remove('d-none');
+                    header.classList.remove('d-none');
+                    this.newUI.displaygames(result);
+                    this.showdetails(result); 
+                    
+                });
+            }
+        });
+    });
+
+
+}
+
+}
+
